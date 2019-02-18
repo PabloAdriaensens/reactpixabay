9247e77cc20e434993f75e4a51a38b3517ed49b7
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-18 11:49:38 +0100

    Showing the images result in Imagen component

--- src/componentes/Imagen.js
+++ src/componentes/Imagen.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const Imagen = () => {
+const Imagen = (props) => {
+
+    const {largeImageUrl, likes, previewURL, tags, views} = props.imagen;
+
     return (
-        <p>desde imagen</p>
+        <div className="col-6 col-sm-4 col-md-3 mb-4">
+            <div className="card">
+                <img className="card-img-top" src={previewURL} alt={tags}/>
+                <div className="card-body">
+                    <p className="card-text">{likes} Me Gusta</p>
+                    <p className="card-text">{views} Vistas</p>
+
+                    <a href={largeImageUrl} target="_blank" rel="noopener noreferrer"
+                       className="btn btn-primary btn-block">Ver Imagen</a>
+                </div>
+            </div>
+        </div>
     )
 };
 
